f65e311084d211d2d5860616772c50e8ff20bb82
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-01-19 02:41:27 -0200

    [UPD] 19/01/2018

--- lamp.sh
+++ lamp.sh
@@ -43,10 +43,6 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php 
 sudo apt-get update
-echo "\n"
-echo "|----------------------------------------------------|"
-echo "##### => Instalando o PHP 7.2"
-echo "|----------------------------------------------------|"
 sudo apt-cache search php7.2
 sudo apt-get install -y php7.2-cli libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-dev php7.2-mcrypt php7.2-sqlite3 php7.2-mbstring php7.2-gd php7.2-json php7.2-xml php7.2-zip
 php --ini
