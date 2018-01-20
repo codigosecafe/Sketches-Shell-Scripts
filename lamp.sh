14794ad85e77920438d4981a5d6d792b8c7bcc58
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-01-20 00:08:38 -0200

    ATUALIZANDO DEFININDO PHP7.1 PARA SCRIPT LAMP

--- lamp.sh
+++ lamp.sh
@@ -10,6 +10,7 @@ echo "# date        : 18/01/2018"
 echo "# version     : 1.0"
 echo "#=============================================================================="
 echo "\n"
+cd ~
 echo "|----------------------------------------------------|"
 echo "##### => Lendo e atualizando os pacotes do sistema"
 echo "|----------------------------------------------------|"
@@ -24,7 +25,7 @@ echo "\n"
 echo "|----------------------------------------------------------------------------------------------|"
 echo "##### => Instalando alguns pacotes que serão necessários para realizar nossa configuração."
 echo "|----------------------------------------------------------------------------------------------|"
-apt-get install -y software-properties-common python-software-properties curl unzip mcrypt
+apt-get install -y software-properties-common python-software-properties curl unzip mcrypt git
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => instalando MySQL"
@@ -35,25 +36,23 @@ echo "|----------------------------------------------------|"
 echo "##### => Instalando o Apache"
 echo "|----------------------------------------------------|"
 sudo apt-get install apache2 -y
+sudo a2enmod rewrite 
 echo "\n"
 echo "|----------------------------------------------------|"
-echo "##### => Instalando o PHP 7.2"
+echo "##### => Instalando o PHP 7.1"
 echo "|----------------------------------------------------|"
-sudo add-apt-repository -y ppa:ondrej/php 
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
-sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php 
-sudo apt-cache search php7.2
-sudo apt-get update
-sudo apt-get install -y php7.2-cli libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-dev php7.2-mcrypt php-mcrypt php7.2-sqlite3 php7.2-mbstring php7.2-gd php7.2-json php7.2-xml php7.2-zip
-sudo apt-cache search php7.2
+sudo apt-get install php7.1 php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
+sudo apt-cache search php7.1
 php --ini
-
+sudo service apache2 restart
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalar o Composer"
 echo "|----------------------------------------------------|"
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 echo "\n"
 # Opcional
 #echo "instalando o phpMyAdmin"
@@ -62,8 +61,7 @@ echo "\n"
 echo "#=============================================================================="
 echo "# AMBIENTE DE DESENVOLVIMENTO CRIADO"
 echo "#=============================================================================="
-sudo a2enmod rewrite 
-sudo systemctl restart apache2
+
 echo '<?php phpinfo(); ?>' > /var/www/html/infophp.php
 xdg-open http://localhost/infophp.php
 apache2 -v
