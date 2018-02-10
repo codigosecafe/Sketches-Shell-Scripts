bd9143a0bea0e4bdc364946f7f70561218f7eba4
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-02-10 15:11:20 -0200

    [UPD] - ADD PHPMYADMIN

--- LAMP-CECSERVER.sh
+++ LAMP-CECSERVER.sh
@@ -16,6 +16,8 @@ echo "##### => Lendo e atualizando os pacotes do sistema"
 echo "|----------------------------------------------------|"
 env -i sudo apt-get update 
 env -i sudo apt-get -y upgrade 
+echo "# => instalando aptitude "
+env -i sudo apt-get install -y aptitude
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Atualizando a distribução do sistema"
@@ -25,20 +27,20 @@ echo "\n"
 echo "|----------------------------------------------------------------------------------------------|"
 echo "##### => Instalando alguns pacotes que serão necessários para realizar nossa configuração."
 echo "|----------------------------------------------------------------------------------------------|"
-env -i sudo apt-get install -y software-properties-common python-software-properties build-essential libssl-dev libmaxminddb-dev libncursesw5-dev libglib2.0-dev libgeoip-dev libtokyocabinet-dev
-env -i sudo apt-get install -y curl unzip mcrypt git lynx vim aptitude
+env -i sudo aptitude install -y software-properties-common python-software-properties build-essential libssl-dev libmaxminddb-dev libncursesw5-dev libglib2.0-dev libgeoip-dev libtokyocabinet-dev
+env -i sudo aptitude install -y curl unzip mcrypt git lynx vim 
 env -i sudo set -gx TERM "xterm-256color" set -xU LSCOLORS 'di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
 env -i sudo export TERM=xterm-256color
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => instalando MySQL"
 echo "|----------------------------------------------------|"
-env -i sudo apt-get install mysql-server mysql-client -y
+env -i sudo aptitude install mysql-server mysql-client -y
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalando o Apache"
 echo "|----------------------------------------------------|"
-env -i sudo apt-get install apache2 -y
+env -i sudo aptitude install apache2 -y
 echo "##### => MELHORANDO SEGURANÇA DO APACHE"
 env -i sudo vim /etc/apache2/conf-available/security.conf
 env -i sudo a2enmod rewrite 
@@ -49,9 +51,9 @@ echo "|----------------------------------------------------|"
 echo "##### => Instalando o PHP 7.1"
 echo "|----------------------------------------------------|"
 env -i sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
-env -i sudo apt-get update
-env -i sudo apt-get install php7.1 php7.1-common -y
-env -i sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
+env -i sudo aptitude update
+env -i sudo aptitude install php7.1 php7.1-common -y
+env -i sudo aptitude install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
 env -i sudo apt-cache search php7.1
 env -i php --ini
 env -i sudo /etc/init.d/apache2 restart
@@ -65,8 +67,8 @@ env -i sudo mv composer.phar /usr/local/bin/composer
 echo "\n"
 # Opcional
 echo "instalando o phpMyAdmin"
-env -i sudo apt-get update
-env -i sudo apt-get install phpmyadmin php-mbstring php-gettext
+env -i sudo aptitude update
+env -i sudo aptitude install phpmyadmin php-mbstring php-gettext
 env -i sudo phpenmod mcrypt
 env -i sudo phpenmod mbstring
 env -i sudo systemctl restart apache2
