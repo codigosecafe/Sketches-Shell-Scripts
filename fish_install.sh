661e22b3731de0e39e461c95005aac6140b5a4d4
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-02-10 16:29:30 -0200

    [UPD] - INSTALL FISH

--- fish_install.sh
+++ fish_install.sh
@@ -23,11 +23,6 @@ echo "##### => Adcionando fish ao sistema"
 env -i sudo apt-add-repository ppa:fish-shell/release-2 -y
 env -i sudo apt-get update
 env -i sudo apt-get install fish -y
-env -i sudo rm .bashrc
-
-
-
-
 
 env -i sudo set -gx TERM "xterm-256color" set -xU LSCOLORS 'di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
 env -i sudo export TERM=xterm-256color
