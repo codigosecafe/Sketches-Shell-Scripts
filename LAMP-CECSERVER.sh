7f7e86ec1c1da5b1837caabb2eb1898704a105d0
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-02-10 21:44:20 -0200

    [UPD] - LAMP

--- LAMP-CECSERVER.sh
+++ LAMP-CECSERVER.sh
@@ -63,6 +63,19 @@ sudo vim /etc/apache2/conf-available/security.conf
 sudo a2enmod rewrite 
 sudo a2enmod deflate
 sudo /etc/init.d/apache2 restart
+echo "|----------------------------------------------------|"
+echo "##### => Instalando o PHP 7.1"
+echo "|----------------------------------------------------|"
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php --assume-yes --force-yes
+cd ~/
+env -i sudo apt-get update
+env -i sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
+env -i sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-cgi php7.1-mysql php-pear php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml --assume-yes --force-yes
+
+sudo apt-cache search php7.1
+env -i php --ini
+sudo /etc/init.d/apache2 restart
+
 
 echo "|----------------------------------------------------|"
 echo "##### => Instalar o Composer"
@@ -74,35 +87,17 @@ echo "\n"
 # Opcional
 echo "instalando o phpMyAdmin"
 env -i sudo apt-get update
-env -i sudo apt-get install dbconfig-common dbconfig-mysql libjs-jquery libjs-sphinxdoc libjs-underscore php-gettext php-mbstring php-pear php-phpseclib php7.2-common php7.2-mbstring dbconfig-common dbconfig-mysql libjs-jquery libjs-sphinxdoc libjs-underscore php-gettext php-mbstring php-pear php-phpseclib php7.2-common php7.2-mbstring phpmyadmin --assume-yes --force-yes
-env -i sudo apt-get install php-mbstring php-gettext javascript-common php-libsodium php-gmp --assume-yes --force-yes
-env -i sudo apt-get install phpmyadmin --assume-yes --force-yes
+env -i sudo apt-get install dbconfig-common dbconfig-mysql libjs-jquery libjs-sphinxdoc libjs-underscore javascript-common php-gettext --assume-yes --force-yes
+env -i sudo apt-get install phpmyadmin
 env -i sudo phpenmod mcrypt
 env -i sudo phpenmod mbstring
 sudo /etc/init.d/apache2 restart
 echo "\n"
-echo "|----------------------------------------------------|"
-echo "##### => Instalando o PHP 7.1"
-echo "|----------------------------------------------------|"
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php --assume-yes --force-yes
-cd ~/
-env -i sudo apt-get update
-env -i sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
-env -i sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml --assume-yes --force-yes
 
-sudo apt-cache search php7.1
-env -i php --ini
-sudo /etc/init.d/apache2 restart
-echo "\n"
-cd ~/
-env -i sudo apt-get update 
-env -i sudo apt-get --assume-yes --force-yes upgrade 
-env -i sudo apt-get --assume-yes --force-yes clean
-env -i sudo apt-get --assume-yes --force-yes autoclean
-echo "\n"
 echo "#=============================================================================="
 echo "# ADICIONANDO MODELO PADROA PARA VHOST"
 echo "#=============================================================================="
+cd ~/
 env -i echo '<VirtualHost *:80>
         ServerAdmin webmaster@localhost
 
@@ -144,10 +139,19 @@ env -i echo '<VirtualHost *:80>
 </VirtualHost>' > ~/padraoCECSERVER.conf
 env -i sudo mv padraoCECSERVER.conf /etc/apache2/sites-available/
 
+
+
 echo "\n"
 echo "#=============================================================================="
 echo "# AMBIENTE DE DESENVOLVIMENTO CRIADO"
 echo "#=============================================================================="
+echo "\n"
+cd ~/
+env -i sudo apt-get update 
+env -i sudo apt-get --assume-yes --force-yes upgrade 
+env -i sudo apt-get --assume-yes --force-yes clean
+env -i sudo apt-get --assume-yes --force-yes autoclean
+echo "\n"
 
 env -i echo '<?php phpinfo(); ?>' > ~/cecphp.php
 env -i sudo mv cecphp.php /var/www/html/
