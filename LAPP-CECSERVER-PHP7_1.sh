bd88a81a70f6786b4c0df25f9a22b9184038471f
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-07-25 13:57:33 -0300

    ADCIONANDO CONFIGURANÇÃO LAPP

--- LAPP-CECSERVER-PHP7_1.sh
+++ LAPP-CECSERVER-PHP7_1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "\n\n"
 echo "#=============================================================================="
-echo "# title       : LAMP UBUNTU >= 16 PHP7.1"
-echo "# description : INSTALANDO O AMBIENTE PARA DESENVOLVIMENTO LINUX APACHE MySQL PHP (LAMP)."
+echo "# title       : LAPP UBUNTU >= 16 PHP7.1"
+echo "# description : INSTALANDO O AMBIENTE PARA DESENVOLVIMENTO LINUX APACHE POSTGRESQL PHP. (LAPP) "
 echo "# author      : Claudio Alexssandro Lino"
 echo "# site        : http://cecdigitalmaker.com.br"
 echo "# github      : https://github.com/codigosecafe"
 echo "# date        : 25/07/2018"
-echo "# version     : 3.2"
+echo "# version     : 2.1"
 echo "#=============================================================================="
 echo "\n"
-
+cd ~/
 echo "|----------------------------------------------------|"
 echo "##### => Lendo e atualizando os pacotes do sistema"
 echo "|----------------------------------------------------|"
@@ -29,29 +29,21 @@ sudo apt-get install -y software-properties-common python-software-properties bu
 sudo apt-get install -y curl unzip mcrypt git lynx vim aptitude
 echo "\n"
 echo "|----------------------------------------------------|"
-echo "##### => instalando MySQL 5.7"
+echo "##### => instalando PostgreSQL"
 echo "|----------------------------------------------------|"
-cd ~/
-env -i wget https://dev.mysql.com/get/mysql-apt-config_0.8.9-1_all.deb
-env -i sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
-sudo apt-get update
-sudo apt-get install mysql-server
-env -i mysql_upgrade -u root -p --force
-sudo service mysql stop
-sudo usermod -d /var/lib/mysql/ mysql
-sudo service mysql start
-#env -i sudo apt-get install mysql-server mysql-client -y
+sudo aptitude install -y postgresql libpq5 postgresql-9.5 postgresql-client-9.5 postgresql-client-common postgresql-contrib
+
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalando o Apache"
 echo "|----------------------------------------------------|"
-sudo aptitude install apache2 -y
+sudo apt-get install apache2 apache2-utils -y
 sudo vim /etc/apache2/envvars
 echo "##### => MELHORANDO SEGURANÇA DO APACHE"
 sudo vim /etc/apache2/conf-available/security.conf
-env -i sudo a2enmod rewrite 
-env -i sudo a2enmod deflate
-env -i sudo /etc/init.d/apache2 restart
+sudo a2enmod rewrite 
+sudo a2enmod deflate
+sudo /etc/init.d/apache2 restart
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalando o PHP 7.1"
@@ -61,7 +53,7 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update -y
 sudo apt-cache pkgnames | grep php7.1
 sudo apt-get install php7.1 php7.1-common php-pear -y
-sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
+sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-pgsql php7.1-curl php7.1-json php7.1-soap php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
 sudo apt-cache pkgnames | grep php7.1
 sudo a2dismod php7.2 
 sudo a2enmod php7.1 
@@ -81,6 +73,16 @@ sudo apt-get update
 sudo apt-get -y upgrade 
 sudo apt-get -y clean
 sudo apt-get -y autoclean
+echo "\n"
+echo "#=============================================================================="
+echo "# ADICIONANDO PHPPGADMIN"
+echo "#=============================================================================="
+sudo apt install -y phppgadmin
+sudo vim /etc/apache2/conf-available/phppgadmin.conf
+sudo vim /etc/phppgadmin/config.inc.php
+sudo /etc/init.d/postgresql restart
+sudo /etc/init.d/apache2 restart
+
 echo "\n"
 echo "#=============================================================================="
 echo "# ADICIONANDO MODELO PADROA PARA VHOST"
@@ -137,7 +139,7 @@ echo "# visualizar PHP INFO:   http://localhost/cecphp.php"
 
 apache2 -v
 php -v
-mysql -V
+pg_config --version
 echo "\n"
 echo "#=========================== FIM DO SCRIPT ===================================="
 echo "\n\n"
