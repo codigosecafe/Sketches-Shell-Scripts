717d00ba261b64bc1ae60b20253bbe86dd5c75d8
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2018-11-05 16:28:38 -0200

    ATUALIZANDO

--- INSTALL_FAIL2BAN.sh
+++ INSTALL_FAIL2BAN.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "\n\n"
 echo "#=============================================================================="
-echo "# title       : Fish shell"
-echo "# description : Instalando uma alternativa configurável e fácil de usar para o Bash."
+echo "# title       : INSTALL FAIL2BAN"
+echo "# description : INSTALL FAIL2BAN."
 echo "# author      : Claudio Alexssandro Lino"
 echo "# site        : http://codigosecafe.com.br"
 echo "# github      : https://github.com/codigosecafe"
@@ -10,7 +10,6 @@ echo "# date        : 18/01/2018"
 echo "# version     : 1.0"
 echo "#=============================================================================="
 echo "\n"
-
 echo "##### => Lendo os pacotes disponiveis"
 sudo apt-get update
 echo "\n"
@@ -19,17 +18,19 @@ sudo apt-get upgrade -y
 echo "# => instalando aptitude "
 sudo apt-get install -y aptitude
 echo "\n"
-echo "##### => Adcionando fish ao sistema"
-# sudo apt-add-repository ppa:fish-shell/release-2 -y
-sudo aptitude update
-sudo aptitude install fish -y
+echo "# => instalando FAIL2BAN "
+sudo aptitude install fail2ban
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
-echo "\n"
-echo "##### => Defenindo o fish como padrao"
-sudo chsh -s /usr/bin/fish
-echo "\n"
-echo "Iniciando o fish"
-fish
+sudo vim /etc/fail2ban/jail.local
+
+sudo systemctl service enable fail2ban
+sudo systemctl service start fail2ban
+fail2ban-client restart
+fail2ban-client status
+#sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#sudo /etc/init.d/mysql restart
+
 echo "\n"
 echo "#=========================== FIM DO SCRIPT ===================================="
 echo "\n\n"
--- UNB18_LAMP-CECSERVER-PHP7_1.sh
+++ UNB18_LAMP-CECSERVER-PHP7_1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "\n\n"
 echo "#=============================================================================="
-echo "# title       : LAPP UBUNTU >= 16 PHP7.1"
-echo "# description : INSTALANDO O AMBIENTE PARA DESENVOLVIMENTO LINUX APACHE POSTGRESQL PHP. (LAPP) "
+echo "# title       : LAMP UBUNTU >= 16 PHP7.1"
+echo "# description : INSTALANDO O AMBIENTE PARA DESENVOLVIMENTO LINUX APACHE MySQL PHP (LAMP)."
 echo "# author      : Claudio Alexssandro Lino"
 echo "# site        : http://cecdigitalmaker.com.br"
 echo "# github      : https://github.com/codigosecafe"
 echo "# date        : 25/07/2018"
-echo "# version     : 2.1"
+echo "# version     : 3.2"
 echo "#=============================================================================="
 echo "\n"
-cd ~/
+
 echo "|----------------------------------------------------|"
 echo "##### => Lendo e atualizando os pacotes do sistema"
 echo "|----------------------------------------------------|"
@@ -25,35 +25,44 @@ echo "\n"
 echo "|----------------------------------------------------------------------------------------------|"
 echo "##### => Instalando alguns pacotes que serão necessários para realizar nossa configuração."
 echo "|----------------------------------------------------------------------------------------------|"
-sudo apt-get install -y software-properties-common python-software-properties build-essential libssl-dev
+sudo apt --fix-broken install python-pycurl python-apt
+sudo apt-get install -y software-properties-common build-essential libssl-dev
 sudo apt-get install -y curl unzip mcrypt git lynx vim aptitude
 echo "\n"
 echo "|----------------------------------------------------|"
-echo "##### => instalando PostgreSQL"
+echo "##### => instalando MySQL 5.7"
 echo "|----------------------------------------------------|"
-sudo aptitude install -y postgresql libpq5 postgresql-9.5 postgresql-client-9.5 postgresql-client-common postgresql-contrib
-
+cd ~/
+env -i wget https://dev.mysql.com/get/mysql-apt-config_0.8.9-1_all.deb
+env -i sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
+sudo apt-get update
+sudo apt-get install -y mysql-server
+env -i mysql_upgrade -u root -p --force
+sudo service mysql stop
+sudo usermod -d /var/lib/mysql/ mysql
+sudo service mysql start
+#env -i sudo apt-get install mysql-server mysql-client -y
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalando o Apache"
 echo "|----------------------------------------------------|"
-sudo apt-get install apache2 apache2-utils -y
+sudo aptitude install apache2 -y
 sudo vim /etc/apache2/envvars
 echo "##### => MELHORANDO SEGURANÇA DO APACHE"
 sudo vim /etc/apache2/conf-available/security.conf
-sudo a2enmod rewrite 
-sudo a2enmod deflate
-sudo /etc/init.d/apache2 restart
+env -i sudo a2enmod rewrite 
+env -i sudo a2enmod deflate
+env -i sudo /etc/init.d/apache2 restart
 echo "\n"
 echo "|----------------------------------------------------|"
 echo "##### => Instalando o PHP 7.1"
 echo "|----------------------------------------------------|"
-sudo apt-get install -y software-properties-common
+sudo aptitude install -y software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get update -y
+sudo aptitude update -y
 sudo apt-cache pkgnames | grep php7.1
-sudo apt-get install php7.1 php7.1-common php-pear -y
-sudo apt-get install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-pgsql php7.1-curl php7.1-json php7.1-soap php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
+sudo aptitude install php7.1 php7.1-common php-pear -y
+sudo aptitude install php7.1-cli php7.1-gd libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php7.1-zip php7.1-xml -y
 sudo apt-cache pkgnames | grep php7.1
 sudo a2dismod php7.2 
 sudo a2enmod php7.1 
@@ -73,16 +82,6 @@ sudo apt-get update
 sudo apt-get -y upgrade 
 sudo apt-get -y clean
 sudo apt-get -y autoclean
-echo "\n"
-echo "#=============================================================================="
-echo "# ADICIONANDO PHPPGADMIN"
-echo "#=============================================================================="
-sudo apt install -y phppgadmin
-sudo vim /etc/apache2/conf-available/phppgadmin.conf
-sudo vim /etc/phppgadmin/config.inc.php
-sudo /etc/init.d/postgresql restart
-sudo /etc/init.d/apache2 restart
-
 echo "\n"
 echo "#=============================================================================="
 echo "# ADICIONANDO MODELO PADROA PARA VHOST"
@@ -139,7 +138,7 @@ echo "# visualizar PHP INFO:   http://localhost/cecphp.php"
 
 apache2 -v
 php -v
-pg_config --version
+mysql -V
 echo "\n"
 echo "#=========================== FIM DO SCRIPT ===================================="
 echo "\n\n"
