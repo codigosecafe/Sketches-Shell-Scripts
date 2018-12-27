bd57d0c484b4761daf9968b359ed1a30e18979e8
Claudio Alexssandro Lino <web@condor.com.br>
2018-12-27 13:04:28 -0200

    Melhorando as coisas

--- resouces/views/about.sh
+++ resouces/views/about.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "\n\n"
-echo "#=============================================================================="
-echo "# title       : SHELL SCRIPT -> CONFIG SERVER"
-echo "# description : INSTALA/CONFIGURA O AMBIENTE PARA DESENVOLVIMENTO WEB EM AMBIENTE LINUX/UBUNTU."
-echo "# author      : Claudio Alexssandro Lino"
-echo "# github      : https://github.com/codigosecafe/meus-shell-scripts/"
-echo "# version     : 18.12.27"
-echo "#=============================================================================="
-echo "\n"
+clear
+echo "#================================================================================================"
+echo "# title       : SHELL SCRIPT -> CONFIG SERVER                                                   #"
+echo "# description : INSTALA/CONFIGURA O AMBIENTE PARA DESENVOLVIMENTO WEB EM AMBIENTE LINUX/UBUNTU. #"
+echo "# author      : Claudio Alexssandro Lino                                                        #"
+echo "# github      : https://github.com/codigosecafe/meus-shell-scripts/                             #"
+echo "# version     : 18.12.27                                                                        #"                                                       
+echo "#================================================================================================"
+
 
