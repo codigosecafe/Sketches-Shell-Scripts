00532b6baf75c186e0eb320dccb5278f90c208d3
Claudio Alexssandro Lino <web@condor.com.br>
2018-12-27 13:01:57 -0200

    Melhorando as coisas

--- resouces/views/exit-view.sh
+++ resouces/views/exit-view.sh
@@ -3,5 +3,6 @@ view_close(){
    clear
    msg="Obrigado por usar o script, quero que ele se torne algo util para muitos desenvolvedoress."
    whiptail --title "$TITLE_APP" --msgbox "$msg" 10 60
+   source ./resouces/views/about.sh 
    exit
 }
