5ea0a56cced2fdbc11342d60897c5c138e2cc853
Claudio Alexssandro Lino <web@condor.com.br>
2018-12-27 13:17:44 -0200

    ADD ABOUT LOAD

--- resouces/scripts/load_files.sh
+++ resouces/scripts/load_files.sh
@@ -6,8 +6,11 @@ source ./resouces/scripts/install_resources.sh
 
 
 # CARREGA AS VIEWS
-source ./resouces/views/home-view.sh
-source ./resouces/views/exit-view.sh
-source ./resouces/views/menu-view.sh
-source ./resouces/views/menu-recommendations-view.sh
+source ./resouces/views/about.sh
 source ./resouces/views/cancel-view.sh
+source ./resouces/views/exit-view.sh
+source ./resouces/views/home-view.sh
+source ./resouces/views/menu-recommendations-view.sh
+source ./resouces/views/menu-view.sh
+
+
