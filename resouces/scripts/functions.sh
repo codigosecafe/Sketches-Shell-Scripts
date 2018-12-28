dcfa5207f9ff3a71683a2369c701250b6643ffcc
Claudio Alexssandro Lino <web@condor.com.br>
2018-12-28 11:40:22 -0200

    UPD 4

--- resouces/scripts/functions.sh
+++ resouces/scripts/functions.sh
@@ -134,6 +134,7 @@ fn_install_MySQL(){
     cd ~/
     sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
     sudo add-apt-repository 'deb [arch=amd64] http://mirror.zol.co.zw/mariadb/repo/10.3/ubuntu bionic main'
+    sudo dpkg --configure -a
     sudo apt update
     sudo apt install mariadb-server mariadb-client -y
 
