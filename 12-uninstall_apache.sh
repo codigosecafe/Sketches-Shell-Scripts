92758692971c1c06607a1ede2cbf66342591e072
Claudio Alexssandro Lino <claudio.lino@tecnofit.com.br>
2022-08-31 11:09:27 -0300

    [MASTER]-[task]

--- 12-uninstall_apache.sh
+++ 12-uninstall_apache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "\n\n"
 echo "#=============================================================================="
-echo "# title       : UNINSTALL PHP"
-echo "# description : Script para remover do PHP"
+echo "# title       : UNINSTALL Apache2"
+echo "# description : Script para remover do Apache2"
 echo "# author      : Claudio Alexssandro Lino"
 echo "# github      : https://github.com/codigosecafe"
 echo "# version     : 1.0.0"
